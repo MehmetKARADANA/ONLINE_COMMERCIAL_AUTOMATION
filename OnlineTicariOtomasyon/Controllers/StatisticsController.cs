97018efc6ff000bba54df824b797dfdb156aa66d
Mehmet Karadana <karadanam519@gmail.com>
2024-04-27 15:44:01 +0300

    commit 2
    Add statistic charts.

---
+++ OnlineTicariOtomasyon/Controllers/StatisticsController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var value1=context.Currents.Count().ToString();
+            ViewBag.d1=value1;
+
+            var value2 = context.Products.Count().ToString();
+            ViewBag.d2 = value2;
+
+            var value3 = context.Employees.Count().ToString();
+            ViewBag.d3 = value3;
+
+            var value4 = context.Categories.Count().ToString();
+            ViewBag.d4 = value4;
+            return View();
+        }
+
+
+    }
+}
